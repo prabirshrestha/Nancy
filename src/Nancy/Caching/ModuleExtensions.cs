bd920d0ef2b9d860e02174acc93e4a4921f61fa1
prabirshrestha <mail@prabir.me>
2012-03-08 18:52:59 -0500

    added Caching.CheckForIfNonMatch and Caching.CheckForIfModifiedSince module extensions (#183)

---
+++ src/Nancy/Caching/ModuleExtensions.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Nancy.Caching
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    public static class ModuleExtensions
+    {
+        public static void CheckForIfNonMatch(this NancyModule module)
+        {
+            module.After.AddItemToEndOfPipeline(CheckForInNonMatch);
+        }
+
+        public static void CheckForIfModifiedSince(this NancyModule module)
+        {
+            module.After.AddItemToEndOfPipeline(CheckForIfModifiedSince);
+        }
+
+        private static void CheckForInNonMatch(NancyContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            string responseETag;
+            if (response.Headers.TryGetValue("ETag", out responseETag))
+            {
+                if (request.Headers.IfNoneMatch.Contains(responseETag))
+                {
+                    context.Response = HttpStatusCode.NotModified;
+                }
+            }
+        }
+
+        private static void CheckForIfModifiedSince(NancyContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            string responseLastModified;
+            if (response.Headers.TryGetValue("Last-Modified", out responseLastModified))
+            {
+                DateTime lastModified;
+
+                if (request.Headers.IfModifiedSince.HasValue && DateTime.TryParseExact(responseLastModified, "R", CultureInfo.InvariantCulture, DateTimeStyles.None, out lastModified))
+                {
+                    if (lastModified <= request.Headers.IfModifiedSince.Value)
+                    {
+                        context.Response = HttpStatusCode.NotModified;
+                    }
+                }
+            }
+        }
+    }
+}
