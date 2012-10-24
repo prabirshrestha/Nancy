e145457a34bc1082280e5d6ebe35b4fde7b043dc
prabirshrestha <mail@prabir.me>
2012-10-23 22:18:56 -0400

    added empty HttpParserStreamExtensions.ReadAsRequest

---
+++ src/Nancy/HttpParser/HttpParserStreamExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Nancy.HttpParser
+{
+    using System;
+    using System.IO;
+
+    public static class HttpParserStreamExtensions
+    {
+        public static Request ReadAsRequest(this Stream stream, string scheme = "http")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
