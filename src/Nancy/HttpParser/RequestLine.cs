1f4af33f23fb5c446b7744f58f2eb46aebb24ae5
prabirshrestha <mail@prabir.me>
2012-10-23 22:15:24 -0400

    added RequestLine

---
+++ src/Nancy/HttpParser/RequestLine.cs
@@ -0,0 +1,31 @@
+﻿namespace Nancy.HttpParser
+{
+    internal class RequestLine
+    {
+        private readonly string method;
+        private readonly string uri;
+        private readonly string httpVersion;
+
+        public RequestLine(string method, string uri, string httpVersion)
+        {
+            this.method = method;
+            this.uri = uri;
+            this.httpVersion = httpVersion;
+        }
+
+        public string HttpVersion
+        {
+            get { return httpVersion; }
+        }
+
+        public string Uri
+        {
+            get { return uri; }
+        }
+
+        public string Method
+        {
+            get { return method; }
+        }
+    }
+}
